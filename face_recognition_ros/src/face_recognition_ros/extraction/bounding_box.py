b1d44a69fe1c00747ff0f7eca94dda90f6266f07
Samuel <sambalmol@uma.es>
2019-06-19 17:32:56 +0200

    Changed bounding box conditions

--- face_recognition_ros/src/face_recognition_ros/extraction/bounding_box.py
+++ face_recognition_ros/src/face_recognition_ros/extraction/bounding_box.py
@@ -38,37 +38,76 @@ def extract_bounding_boxes(datum, confidence_threshold=0.01):
     return bbs
 
 
-def extract_corners(datum, confidence_threshold=0.0):
+def extract_corners(datum, confidence_threshold=0.00):
     bbs = []
-    for person in datum.poseKeypoints:
-        if np.all(person[[16, 17, 0], 2] > confidence_threshold):
+    for p in datum.poseKeypoints:
+        pres = p[:, 2] > confidence_threshold
+
+        if pres[17] and pres[16]:
             # Best condition: two sides present
             ratio = 20.9 / 16.1
+
             # Side to side vector
-            v = person[17, 0:2] - person[16, 0:2]
-            vt = -ratio * v[::-1]
+            v = (p[17, 0:2] - p[16, 0:2]) / 2
+            vt = ratio * v[::-1]
+            vt[0] *= -1
 
             # Corners
-            center = np.mean(person[[0, 16, 17, 14, 15], 0:2], axis=0)
-            topl = center + (vt - v) / 2
-            botr = center + (v - vt) / 2
-            topr = center + (v + vt) / 2
-            botl = center - (v + vt) / 2
-            bb = np.float32([topl, topr, botl, botr])
+            # center = np.mean(p[[0, 16, 17, 14, 15], 0:2], axis=0)
+            topl = p[16, 0:2] - vt
+            botr = p[17, 0:2] + vt
+            topr = p[17, 0:2] - vt
+            botl = p[16, 0:2] + vt
+        elif pres[14] and pres[15] and pres[0] and pres[1]:
+            if pres[16]:
+                orig = p[16, 0:2]
+                d = -1
+            elif pres[17]:
+                orig = p[17, 0:2]
+                d = 1
+            else:
+                return
 
-            bbs.append(bb)
-        elif np.all(person[[15, 17, 0], 2] > confidence_threshold) or np.all(
-            person[[14, 16, 0], 2] > confidence_threshold
-        ):
-            # Worse condition: one side only
-            leftie = np.all(person[[15, 17, 0], 2] > confidence_threshold)
-            pts = [16, 0, 14, 15] if leftie else [17, 0, 15, 14]
+            rem_idx = np.array([0, 14, 15])
+            rem = p[rem_idx, 0:2]
 
-            center = np.mean(person[pts, 0:2], axis=0)
-            leftmost_idx = np.argmax(
-                cdist(person[pts[0], 0:2], person[pts[1:], 0:2])
-            )
+            furth = np.argmax(np.sum((orig - rem) ** 2, axis=1))
 
-            v = person[pts[0], 0:2] - person[leftmost_idx, 0:2]
+            # print(np.sum((p[[pts[0]], 0:2] - p[pts[1:], 0:2])**2, axis=1))
+            v = rem[1] - rem[2]
+            vs = np.linalg.norm(v)
+            v /= vs
+
+            vt = np.copy(v[::-1])
+            vt[1] = vt[1] * -1
+
+            upvec = (p[0, 0:2] - p[1, 0:2]) / 2
+
+            a = orig
+            b = -1.2 * upvec + rem[0]
+            c = rem[furth] + d * v * vs / 2
+            d = 1.2 * upvec + rem[0]
+
+            botl = solve_line_intersect(a, b, vt, v)
+            botr = solve_line_intersect(b, c, v, vt)
+            topr = solve_line_intersect(c, d, vt, v)
+            topl = solve_line_intersect(d, a, v, vt)
+        else:
+            continue
+
+        bb = np.float32([topl, topr, botl, botr])
+        bbs.append(bb)
 
     return bbs
+
+
+def solve_line_intersect(p1, p2, a, b):
+    p1 = np.vstack(p1)
+    p2 = np.vstack(p2)
+    A = np.vstack([a, -1 * b])
+    A = A.T
+    B = p2 - p1
+    C = np.linalg.solve(A, B)
+    d = C[0, 0] * np.vstack(a) + p1
+
+    return d.flatten()
